9c1a6800bd5978d67c5fb1e50469c50fd3f2772e
gehadkhaleeed <127341151+gehadkhaleeed@users.noreply.github.com>
2024-06-21 22:34:37 +0300

    Modified Passgen

--- Backend/passgen.py
+++ Backend/passgen.py
@@ -1,14 +1,11 @@
 import string
 import random
-letters = []
 
-for i in range(len(string.ascii_letters)):
-    letters += string.ascii_letters[i]
-letters += 1,2,3,4,5,6,7,8,9,0
-l = len(letters)
+l = len(string.printable)
 def generate(length):
     password = ""
     for i in range(length):
-        x = random.randint(0,l-1)
-        password+= str(letters[x])
+        x = random.randint(0,l-2)
+        password+= str(string.printable[x])
     return password
+
